65b757deb8d7c71b4bdd7b3487c88e8f09120875
Junayed Akbor <junayedakbour@gmail.com>
2026-01-27 15:57:51 +0600

    feat: add the header component and integrate it into main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Header } from "@components/header";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ components/header.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { NavLink } from "@components/nav-link";
+
+const NAV_LINKS = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "#" },
+  { label: "Blog", href: "#" },
+  { label: "Publications", href: "#" },
+  { label: "Photo Gallery", href: "#" },
+  { label: "Contact", href: "#" },
+] as const;
+
+export function Header() {
+  return (
+    <header>
+      <div className="container px-6 h-18 flex items-center">
+        <div>
+          <Link href="/" className="flex flex-col justify-center select-none">
+            <span className="text-xl font-bold uppercase">Dr. Wakil Ahmed</span>
+            <span className="text-[calc(var(--text-xs)-2px)] uppercase leading-none tracking-[0.18em]">
+              Best Hip and Knee Surgeon
+            </span>
+          </Link>
+        </div>
+        <nav className="flex-1 flex justify-end">
+          <ul className="inline-flex items-center justify-center gap-3">
+            {NAV_LINKS.map((link) => (
+              <NavLink key={link.label} href={link.href}>
+                {link.label}
+              </NavLink>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
