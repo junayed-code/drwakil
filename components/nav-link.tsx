f8796b64adff997639c1f0be9377c321195543e5
Junayed Akbor <junayedakbour@gmail.com>
2026-01-27 15:56:59 +0600

    feat: add the `NavLink` component

---
+++ components/nav-link.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { buttonVariants } from "@components/ui/button";
+import { cn } from "@lib/utils";
+
+export function NavLink({
+  className,
+  ...props
+}: React.ComponentProps<typeof Link>) {
+  return (
+    <li>
+      <Link
+        className={cn(
+          buttonVariants({ size: "sm", variant: "ghost" }),
+          "px-2 py-1 h-auto text-foreground/80",
+          className,
+        )}
+        {...props}
+      />
+    </li>
+  );
+}
