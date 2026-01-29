39a8d12655bf83fd6da2ec2e963f9fdb0efc36dd
Junayed Akbor <junayedakbour@gmail.com>
2026-01-29 09:54:03 +0600

    feat: add variants to the NavLink; make the header mobile responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { Menu, Phone, X } from "lucide-react";
+
+import { Button } from "@components/ui/button";
 import { NavLink } from "@components/nav-link";
-import { Button } from "./ui/button";
-import { Phone } from "lucide-react";
 
 const NAV_LINKS = [
   { label: "Home", href: "/" },
@@ -13,13 +17,16 @@ const NAV_LINKS = [
 ] as const;
 
 export function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
     <header>
       <div className="h-(--header-height)" />
       <div className="fixed top-0 inset-x-0 z-50 h-(--header-height)">
         <div className="backdrop-blur-xl absolute inset-0" />
-        <div className="h-(--header-height) backdrop-opacity-100">
-          <nav className="container mx-auto px-6 lg:px-8 flex items-center h-full text-primary-foreground">
+
+        <nav aria-label="Main">
+          <div className="container mx-auto px-6 lg:px-8 flex items-center h-(--header-height) backdrop-opacity-100 text-primary-foreground">
             <div className="flex-1">
               <Link
                 href="/"
@@ -34,7 +41,7 @@ export function Header() {
               </Link>
             </div>
 
-            <div className="flex items-center gap-4">
+            <div className="hidden lg:flex items-center gap-4">
               <ul className="inline-flex items-center justify-center gap-3">
                 {NAV_LINKS.map((link) => (
                   <NavLink key={link.label} href={link.href}>
@@ -55,8 +62,55 @@ export function Header() {
                 </Link>
               </Button>
             </div>
-          </nav>
-        </div>
+
+            {/* Mobile navbar toggle button */}
+            <Button
+              size="icon"
+              variant="outline"
+              aria-label="Toggle menu"
+              className="lg:hidden bg-transparent border-transparent shadow-[inset_0_0_2px_rgba(255,255,255,0.6)]"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            >
+              {isMobileMenuOpen ? (
+                <X className="h-6 w-6" />
+              ) : (
+                <Menu className="h-6 w-6" />
+              )}
+            </Button>
+          </div>
+
+          {/* Mobile nav */}
+          {isMobileMenuOpen && (
+            <div className="lg:hidden absolute w-full z-40 top-(--header-height) backdrop-blur-xl bg-card/55">
+              <div className="container mx-auto flex flex-col py-5 gap-2">
+                {/* Mobile nav links */}
+                <ul className="flex flex-col gap-1 px-3">
+                  {NAV_LINKS.map((link) => (
+                    <NavLink
+                      variant="mobile"
+                      key={link.label}
+                      href={link.href}
+                      onClick={() => setIsMobileMenuOpen(false)}
+                    >
+                      {link.label}
+                    </NavLink>
+                  ))}
+                </ul>
+
+                {/* Mobile nav cta */}
+                <div className="px-6">
+                  <div className="w-full border-t border-muted-foreground/50 mt-2 mb-5" />
+                  <Button asChild size="sm" className="w-full">
+                    <Link href="tel:+8809610009630">
+                      <Phone />
+                      <span>(880) 9610-009630</span>
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+        </nav>
       </div>
     </header>
   );
--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -1,19 +1,31 @@
 import Link from "next/link";
-import { buttonVariants } from "@components/ui/button";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@lib/utils";
 
+const navLinkVariants = cva(
+  "flex items-center gap-1 whitespace-nowrap rounded-md text-sm font-medium hover:bg-accent hover:text-accent-foreground transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-3.5 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+  {
+    variants: {
+      variant: {
+        mobile: "px-3 py-2",
+        desktop: "px-2 py-1 justify-center",
+      },
+    },
+    defaultVariants: {
+      variant: "desktop",
+    },
+  },
+);
+
 export function NavLink({
+  variant,
   className,
   ...props
-}: React.ComponentProps<typeof Link>) {
+}: React.ComponentProps<typeof Link> & VariantProps<typeof navLinkVariants>) {
   return (
     <li>
       <Link
-        className={cn(
-          buttonVariants({ size: "sm", variant: "ghost" }),
-          "px-2 py-1 h-auto text-inherit",
-          className,
-        )}
+        className={cn(navLinkVariants({ variant, className }))}
         {...props}
       />
     </li>
